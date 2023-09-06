597fd02db2e53ca8883a1dbed7780943d1ff8d24
geomch <connself@gmail.com>
2023-09-06 12:54:02 +0800

    +

--- x86_64/diy-part1.sh
+++ x86_64/diy-part1.sh
@@ -22,3 +22,5 @@ echo "--------------diy-part1 start--------------"
 
 # sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 # sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+
+sed -i '$a src-git ysx88 https://github.com/ysx88/openwrt-packages' feeds.conf.default
