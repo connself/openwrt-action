3f97a29c4186afeca3a0370d7e4cd897f6c44c59
geomch <connself@163.com>
2024-05-25 14:09:26 +0800

    +

--- x86_64/diy-part1.sh
+++ x86_64/diy-part1.sh
@@ -33,6 +33,8 @@ sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
 rm -rf feeds/luci/applications/luci-app-mosdns
 rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,v2ray*,sing*,smartdns}
 rm -rf feeds/packages/utils/v2dat
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
 
 # 安装feeds 必须
 ./scripts/feeds install -a 
